bcbef77ab558c78c9f7d866d1319520fbdd3790d
sam0406 <sam0406puri@gmail.com>
2026-08-16 22:02:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const menu = [
     category: "Mac & Cheese Bar",
     items: [
       ["Four Cheese Indulgence Mac", "Creamy blend of four melted Italian cheeses in silky pasta perfection.", "549.00"],
-      ["Hot Mushroom Mac", "Sautéed mushrooms and sliced jalapeños in a bold, spicy cheese sauce.", "599.00"]
+      ["Hot Mushroom Mac", "Sautéed mushrooms and sliced jalapeños in a bold, spicy cheese sauce.", "599.00"],
       ["Blaze Mac (Chicken)", "Fiery habanero-spiced chicken folded into ultra-creamy cheese mac.", "599.00"],
       ["Pizza Mac Mashup", "Cheesy mac meets pepperoni/Garden fresh vegetables , tomato sauce with oregano and marinara swirl.", "549.00 / 599.00"],
       ["Birria Mac (Lamb)", "Rich braised birria lamb stirred through velvety three-cheese macaroni.", "649.00"],
