c83b18f091e96df414ed426f12b8fe1fcbdc7943
sam0406 <sam0406puri@gmail.com>
2026-08-19 14:30:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const menu = [
         "(Spicy Chicken / Truffle Mushroom / Veg Fiesta) Golden deep-fried tortilla stuffed with savoury mushroom, veggie or chicken filling.",
         "549.00 / 649.00", "mixed" ],
       ["Habanero Chicken Bites", "Juicy chicken ignited with smoky habanero chilli and bold spices.", "549.00" , "nonveg"],
+      ["Habanero Crisp Potato", "Crispy potato fries ignited with smoky habanero chilli and bold spices.", "449.00" , "veg"],
       ["Nacho Royale", "Towering crispy nachos crowned with salsas, cheese and fresh toppings.", "499.00" , "veg"],
       ["Wood Fired Garlic Bread", "Artisan bread kissed by woodfire, slathered in roasted garlic butter.", "399.00" , "veg"],
     ],
